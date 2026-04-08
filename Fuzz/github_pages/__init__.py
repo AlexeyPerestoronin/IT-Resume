5db1d0cb22a588225e4c32efcc064125068c60cb
Aleksey Perestoronin <aleksey.perestoronin@myoffice.team>
2026-04-08 16:14:36 +0600

    develop collection of task 'github-pages': temporary-2

--- Fuzz/github_pages/__init__.py
+++ Fuzz/github_pages/__init__.py
@@ -4,14 +4,15 @@ import commandcript
 @commandcript.script_task()
 def prepare_os(ctx):
     """
-    TODO: need provide some description
+    Creates script for complex OS preparing for working with 'bundle'.
+    Note: run manually with sudo access rights.
     """
     commandcript.ScriptExecutor(ctx.script_dir, False)\
         .add_cwd(commandcript.ENV_CONTEXT.PROJECT_GIT_DIR)\
         .add_commands([
-            ['sudo apt update'],
-            ['sudo apt install -y ruby-full build-essential zlib1g-dev'],
-            ['sudo apt update'],
+            ['apt update'],
+            ['apt install -y ruby-full build-essential zlib1g-dev'],
+            ['apt update'],
             ['bundle install'],
             ])\
         .execute(log="github-pages.prepare-os.log")
@@ -20,7 +21,7 @@ def prepare_os(ctx):
 @commandcript.script_task()
 def prepare_gem(ctx):
     """
-    TODO: need provide some description
+    Install 'bundle', 'jekyll' and install the site running environment for local testing.
     """
     commandcript.ScriptExecutor(ctx.script_dir, ctx.launch)\
         .add_cwd(commandcript.ENV_CONTEXT.PROJECT_GIT_DIR)\
@@ -38,8 +39,9 @@ def prepare_gem(ctx):
 @commandcript.script_task()
 def launch_local(ctx):
     """
-    TODO: need provide some description
+    Launch local bundle-server for testing.
     """
+    # TODO: need to implement kill 'bundle'-process optional ability
     commandcript.ScriptExecutor(ctx.script_dir, ctx.launch)\
         .add_cwd(commandcript.ENV_CONTEXT.PROJECT_GIT_DIR)\
         .add_env({
